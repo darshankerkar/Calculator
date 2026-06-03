00aa92d7460d9b084cf307fc576ba01a45db5e88
darshankerkar <darshankerkar09@gmail.com>
2026-06-03 12:07:15 +0530

    Added pylint steps

--- Day_3/Project/Calculator/test_calculator.py
+++ Day_3/Project/Calculator/test_calculator.py
@@ -3,4 +3,4 @@ from calculator import add, subtract
 def test_add():
     assert add(4,5)==9 # asserts whether true or false
     assert add(4,3)==7
-    assert subtract(5,3)==1
+    assert subtract(5,3)==2
